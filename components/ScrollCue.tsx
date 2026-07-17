4f40a8fcb89a7f39421ceeb574b21386d711f7cb
nycocado <nycolascanutto@gmail.com>
2026-07-17 12:56:13 +0100

    [fix] hide scroll cue on mobile viewports

--- components/ScrollCue.tsx
+++ components/ScrollCue.tsx
@@ -7,7 +7,7 @@ export async function ScrollCue() {
     <a
       href="#projects"
       aria-label={t("label")}
-      className="hp-cue-bounce absolute bottom-8 left-1/2 -translate-x-1/2 text-gruvbox-yellow hover:text-gruvbox-yellow-aa transition-colors"
+      className="hp-cue-bounce hidden md:block absolute bottom-8 left-1/2 -translate-x-1/2 text-gruvbox-yellow hover:text-gruvbox-yellow-aa transition-colors"
     >
       <svg
         width={64}
